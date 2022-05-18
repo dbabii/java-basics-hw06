8dad733065f53a5302de86f3f90038328a2eab64
Dmytro Babii <dm.babii@gmail.com>
2022-05-18 10:43:01 +0300

    added test data

--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,8 +14,10 @@ public class TemperatureTest {
     return new Object[][]{
         {212.0, 100.0},
         {32.0, 0.0},
-        {-15, -26.11111111111111}
+        {-15, -26.11111111111111},
         // TODO add 2 more test data here
+        {-90, -67.77777777777777},
+        {-11.2, -24}
     };
   }
 
@@ -28,8 +30,10 @@ public class TemperatureTest {
     return new Object[][]{
         {100.0, 212.0},
         {0.0, 32.0},
-        {-26.11111111111111, -15}
+        {-26.11111111111111, -15},
         // TODO add 2 more test data here
+            {10.0, 50},
+            {4, 39.2}
     };
   }
 
